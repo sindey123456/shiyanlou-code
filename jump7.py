8bb4f8f1706f156cf8a2a3890181a2b3ad365cfe
zechao.zhong <sindey123456@163.com>
2019-08-28 23:20:23 +0800

    jump7 upload test

---
+++ jump7.py
@@ -0,0 +1,11 @@
+num=range(1,101)
+for i in num:
+        if i%7 == 0:
+                    continue
+                    elif i%10 == 7:
+                                continue
+                                elif i//10 ==7:
+                                            continue
+                                            else:
+                                                        print(i)
+
